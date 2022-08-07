ab61f27237c9e6af096ab659e44633560d4d9275
maximiloi <maximiloi@gmail.com>
2022-08-07 13:56:30 +0300

    add menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,3 +1,16 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+
+
+document.addEventListener('click', documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+  // console.log('targetElement: ', targetElement);
+
+  if (targetElement.closest('.menu__link')) {
+    document.documentElement.classList.remove('menu-open');
+    document.documentElement.classList.remove('lock');
+  }
+};
